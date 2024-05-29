a9cbf23d42f3d44efc8638280047330d8d75cd2a
Aleksandr <santurovs866@gmail.com>
2024-05-29 14:10:23 +0500

    add modal for beats.html and pre-listening

--- src/main/java/ru/santurov/paceopp/controllers/BeatController.java
+++ src/main/java/ru/santurov/paceopp/controllers/BeatController.java
@@ -1,13 +1,18 @@
 package ru.santurov.paceopp.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.santurov.paceopp.DTO.BeatDTO;
+import ru.santurov.paceopp.models.Audio;
+import ru.santurov.paceopp.models.Beat;
 import ru.santurov.paceopp.services.BeatService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/beats")
@@ -21,5 +26,23 @@ public class BeatController {
         List<BeatDTO> beats = beatService.findAll().stream().map(beatService::toBeatDTO).toList();
             model.addAttribute("beats", beats);
         return "beats";
+    }
+    @GetMapping("/{id}/audio")
+    @ResponseBody
+    public ResponseEntity<byte[]> getBeatAudio(@PathVariable int id) {
+        Optional<Beat> beatOptional = beatService.findById(id);
+        if (beatOptional.isPresent()) {
+            Beat beat = beatOptional.get();
+            Optional<Audio> mp3Audio = beat.getAudioFiles().stream()
+                    .filter(audio -> "mp3".equalsIgnoreCase(audio.getFileFormat()))
+                    .findFirst();
+            if (mp3Audio.isPresent()) {
+                Audio audio = mp3Audio.get();
+                return ResponseEntity.ok()
+                        .contentType(MediaType.parseMediaType("audio/mpeg"))
+                        .body(audio.getData());
+            }
+        }
+        return ResponseEntity.notFound().build();
     }
 }
